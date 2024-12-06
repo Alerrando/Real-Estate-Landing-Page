39626cdfd4ff2e1b2d44fa2e2e34115f157eb901
Alerrando <alerrando2@gmail.com>
2024-12-06 11:58:17 -0300

    feat:: challenge 2 completed

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 document.addEventListener("DOMContentLoaded", function() {
     challenge1();
+    challenge2();
 });
 
 function challenge1(){
     const h1 = document.querySelector("h1");
-    h1.innerText = "Bem-vindo ao Nosso Site";
+
+    if(h1){
+        h1.innerText = "Bem-vindo ao Nosso Site";
+    }
+}
+
+function challenge2() {
+    const heroContent = document.querySelector(".hero-content");
+
+    if (heroContent) {
+        heroContent.style.backgroundImage = "url('./assets/images/Banner.png')";
+    }
 }
