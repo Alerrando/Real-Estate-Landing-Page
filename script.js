4f30f97b9e229857330e1b4943bc39bd4326530b
Alerrando <alerrando2@gmail.com>
2024-12-06 10:34:23 -0300

    feat: challenge 1 completed

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    challenge1();
+});
+
+function challenge1(){
+    const h1 = document.querySelector("h1");
+    h1.innerText = "Bem-vindo ao Nosso Site";
+}
